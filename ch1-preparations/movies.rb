83fec1f360b6af59d3731c955fc35f002f57f128
Jon Caldwell <7083048+joncaldwell@users.noreply.github.com>
2020-09-11 10:23:19 +0000

    ch1 exercises done

---
+++ ch1-preparations/movies.rb
@@ -0,0 +1,3 @@
+movies = [2015, 2001, 2012]
+
+movies.each { |value| puts value }
---
+++ ch1-preparations/square_floats.rb
@@ -0,0 +1,9 @@
+def square(n)
+  return n * n
+end
+
+puts 39123.41 * 39123.41
+puts 89.123419858 * 89.123419858
+
+puts square(39123.41)
+puts square(89.123419858)
