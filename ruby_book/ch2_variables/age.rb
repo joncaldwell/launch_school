bb939538762297f53755ddd4144cfa2d68521a66
Jon Caldwell <7083048+joncaldwell@users.noreply.github.com>
2020-09-15 10:30:55 +0000

    ch2 exercises done

--- ruby_book/ch2_variables/age.rb
+++ ruby_book/ch2_variables/age.rb
@@ -6,3 +6,4 @@ puts "In 10 years, you'll be " + (age.to_i + 10).to_s + " years old."
 puts "In 20 years, you'll be " + (age.to_i + 20).to_s + " years old."
 puts "In 30 years, you'll be " + (age.to_i + 30).to_s + " years old."
 puts "In 40 years, you'll be " + (age.to_i + 40).to_s + " years old."
+
