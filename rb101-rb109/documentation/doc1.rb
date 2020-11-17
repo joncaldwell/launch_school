5944c1c7d8a2ce58869f6956c0c074384a399383
Jon Caldwell <7083048+joncaldwell@users.noreply.github.com>
2020-11-17 12:25:30 +0000

    adjustments

--- rb101-rb109/documentation/doc1.rb
+++
@@ -1,6 +0,0 @@
-require "date"
-
-puts Date.new               # error, no args
-puts Date.new(2016)         # #<Date: 2016-01-01...
-puts Date.new(2016, 5)      # #<Date: 2016-01-05...
-puts Date.new(2016, 5, 13)  # error, too many args
--- rb101-rb109/easy1/easy1.rb
+++
@@ -1,7 +0,0 @@
-def repeat(str, int)
-  int.times do
-    puts str
-  end
-end
-
-repeat("Hello", 3)
