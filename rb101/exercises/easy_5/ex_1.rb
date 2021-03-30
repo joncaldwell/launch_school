eb9a8edcb4229a2f3b55b16ee7e60b7939c3bf10
Jon Caldwell <7083048+joncaldwell@users.noreply.github.com>
2021-03-30 11:24:51 +0000

    easy_5 exercises

---
+++ rb101/exercises/easy_5/ex_1.rb
@@ -0,0 +1,39 @@
+# MY SOLUTION
+
+def time_of_day(minutes)
+  time_arr_to_time_string(minutes.divmod(60))
+end
+
+def time_arr_to_time_string(time_arr)
+  hour = time_arr[0]
+  minutes = time_arr[1]
+
+  hour += 24 while hour <= 0
+
+  hour -= 24 while hour >= 24
+
+  hour = hour.to_s.prepend('0') unless hour > 9
+  minutes = minutes.to_s.prepend('0') unless minutes > 9
+
+  "#{hour}:#{minutes}"
+end
+
+# LAUNCH SCHOOL SOLUTION
+
+# MINUTES_PER_HOUR = 60
+# HOURS_PER_DAY = 24
+# MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR
+
+# def time_of_day(delta_minutes)
+#   delta_minutes = delta_minutes % MINUTES_PER_DAY
+#   hours, minutes = delta_minutes.divmod(MINUTES_PER_HOUR)
+#   format('%02d:%02d', hours, minutes)
+# end
+
+p time_of_day(0) == '00:00'
+p time_of_day(-3) == '23:57'
+p time_of_day(35) == '00:35'
+p time_of_day(-1437) == '00:03'
+p time_of_day(3000) == '02:00'
+p time_of_day(800) == '13:20'
+p time_of_day(-4231) == '01:29'
