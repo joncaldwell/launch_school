188183d40b80bc313c98a900279fb0ea2121b771
Jon Caldwell <7083048+joncaldwell@users.noreply.github.com>
2021-04-28 11:16:51 +0000

    medium 1 exercises

---
+++ rb101/exercises/medium_1/ex_3.rb
@@ -0,0 +1,32 @@
+def toggle_lights(int)
+  lights = [true] * int
+
+  flip_lights(lights)
+
+  find_on_lights(lights)
+end
+
+def flip_lights(lights_array)
+  count = 2
+
+  while count < (lights_array.length + 1)
+    lights_array.each_with_index do |light, index|
+      lights_array[index] = !light if ((index + 1) % count).zero?
+    end
+
+    count += 1
+  end
+
+  lights_array
+end
+
+def find_on_lights(lights_array)
+  result = []
+  lights_array.each_with_index do |light, index|
+    result << (index + 1) if light
+  end
+  result
+end
+
+p toggle_lights(5)
+p toggle_lights(100)
